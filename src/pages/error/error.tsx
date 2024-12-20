7ff3c5b220f875a44fc080dc43132976f70cb37a
Anastasiia Parnet <nastya.parnet.01@gmail.com>
2024-12-20 19:45:26 +0200

    add content for error page

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
 
 export const Error = () => {
-  return <div>error</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <ExclamationCircleIcon className="w-16 h-16 text-darkRed" />
+      <p className="text-h1">Page not found</p>
+      <Link to="/" className="text-link underline">
+        Return to home page
+      </Link>
+    </div>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        brown: ['Brown', 'sans-serif'],
+      },
+      fontSize: {
+        h1: ['32px', '40px'],
+        link: ['16px', '24px'],
+      },
+      colors: {
+        darkRed: '#872020',
+      },
+    },
   },
   plugins: [],
 };
