96b137f6d195c63ce57e14640ebe19225c5dee0b
Anastasiia Parnet <nastya.parnet.01@gmail.com>
2024-12-24 08:18:42 +0200

    add clear localStorage on exit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router } from 'router/Router';
 
 export const App = () => {
+  useEffect(() => {
+    return localStorage.clear();
+  }, []);
+
   return <Router />;
 };
